eec8941c4fe4ad67c377c156dd93bf2ff47eb25a
Daniel McNamara <danmac8407@gmail.com>
2020-04-28 10:44:14 -0500

    accepted changes

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 require("dotenv").config(); 
-<<<<<<< HEAD
-=======
-
->>>>>>> 25cf192ed51f278aaf1b71fa488febcce5977505
 const server = require("./server");
 
 
@@ -10,8 +6,4 @@ const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
-<<<<<<< HEAD
-}); 
-=======
 });
->>>>>>> 25cf192ed51f278aaf1b71fa488febcce5977505
